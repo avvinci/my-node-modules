f46ad1513a055650e59f1e6c7a433d29171bec51
avvinci <vinciabhinav7@gmail.com>
2019-07-10 17:37:10 +0530

    3 ex done

---
+++ program.js
@@ -0,0 +1,15 @@
+// console.log("HELLO WORLD");
+// console.log(process.argv)
+
+// let sum = 0
+// for(let i=2; i < process.argv.length; i++ ){
+//     sum += Number(process.argv[i])
+// }
+
+// console.log(sum)
+
+var fs = require("fs");
+var b = fs.readFileSync(process.argv[2])
+var s = b.toString()
+var ar  = s.split('\n') ;
+console.log(ar.length -1) ;
