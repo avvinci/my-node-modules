79b8f5a4bf1c4f90a9ab6555373d166de0111b46
avvinci <vinciabhinav7@gmail.com>
2019-07-11 20:44:03 +0530

    multiple requests working

--- quotes.js
+++ quotes.js
@@ -1,19 +1,38 @@
-let site = "https://www.goodreads.com/quotes/widget/90010944-abhinav-vinci?v=2"
+let site = "https://www.goodreads.com/quotes/widget/90010944-abhinav-vinci?v=2";
 
-let https = require('https')
-https.get(site , function callback(response){
-  response.on("data" , function (data){
-    // console.log(s[i])
-    let s  = data.toString() ; 
-    let splits = s.split(';');
-    console.log(splits);
-    let removeSlashes = splits[1].split("\\").join("")
-    let removeTrail = removeSlashes.split('&')
-    let quote = removeTrail[0]
-    console.log(quote);
-    let removeAuthSlashes = splits[3].split("\"")
-    let author = removeAuthSlashes[1].split("quotes\\").join("");
-    console.log(author);
-    
-  })
-})
+let quoteCollection = new Set();
+
+function parseData(data) {
+  let dataString = data.toString();
+  let splits = dataString.split(";");
+  console.log(splits);
+  let removeSlashes = splits[1].split("\\").join("");
+  let removeTrail = removeSlashes.split("&");
+  let quote = removeTrail[0];
+  console.log(quote);
+  quoteCollection.add(quote);
+  let removeAuthSlashes = splits[3].split('"');
+  let author = removeAuthSlashes[1].split("quotes\\").join("");
+  console.log(author);
+}
+
+function getQuotes() {
+  let https = require("https");
+  https.get(site, function callback(response) {
+    response.on("data", function(data) {
+      // console.log(s[i])
+      parseData(data);
+    });
+  });
+}
+getQuotes();
+
+// repeat with the interval of 2 seconds
+let timerId = setInterval(getQuotes, 5000);
+
+// after 5 seconds stop
+setTimeout(() => {
+  //     getQuotes
+  clearInterval(timerId);
+  console.log(quoteCollection);
+}, 20000);
