7d8bd3701ce955c2902dc3cb70c98b916b9b30a8
avvinci <vinciabhinav7@gmail.com>
2019-07-11 21:04:00 +0530

    quotes collection working and writing to file

--- quotes.js
+++ quotes.js
@@ -1,19 +1,28 @@
 let site = "https://www.goodreads.com/quotes/widget/90010944-abhinav-vinci?v=2";
 
+let Normal_length = 6  ; 
 let quoteCollection = new Set();
+let authorCollection = new Array() ; 
 
 function parseData(data) {
   let dataString = data.toString();
   let splits = dataString.split(";");
-  console.log(splits);
+//   console.log(splits);
+  if(splits.length > Normal_length){
+    return; // remains to handle quotes conatining semi-colon
+  }  
   let removeSlashes = splits[1].split("\\").join("");
   let removeTrail = removeSlashes.split("&");
   let quote = removeTrail[0];
   console.log(quote);
+  if(quoteCollection.has(quote)){
+      return ; 
+  }
   quoteCollection.add(quote);
   let removeAuthSlashes = splits[3].split('"');
   let author = removeAuthSlashes[1].split("quotes\\").join("");
   console.log(author);
+  authorCollection.push(author)
 }
 
 function getQuotes() {
@@ -28,11 +37,42 @@ function getQuotes() {
 getQuotes();
 
 // repeat with the interval of 2 seconds
-let timerId = setInterval(getQuotes, 5000);
+let timerId = setInterval(getQuotes, 2000);
 
 // after 5 seconds stop
 setTimeout(() => {
   //     getQuotes
   clearInterval(timerId);
   console.log(quoteCollection);
-}, 20000);
+  writeToCSV() ; 
+}, 30000);
+
+
+function writeToCSV(){
+    let index = 0  
+    quoteCollection.forEach(quote => {
+        appendToCSV(quote,authorCollection[index])
+        index+1;
+    })
+}
+
+function appendToCSV(quote, author){
+    const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+    const csvWriter = createCsvWriter({
+      path: "quotes.csv",
+      header: [
+        { id: "quote", title: "QUOTE" },
+        { id: "author", title: "AUTHOR" }
+      ],
+      append: true
+    });
+  
+    const records = [{ quote, author }];
+  
+    csvWriter
+      .writeRecords(records) // returns a promise
+      .then(() => {
+        console.log("...Done");
+      });
+  
+}
