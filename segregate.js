d233cb145c91a588935ff15d49269eafa2112421
avvinci <vinciabhinav7@gmail.com>
2019-07-19 11:15:46 +0530

    file move to seperate dir refactored

--- segregate.js
+++ segregate.js
@@ -1,14 +1,7 @@
 var fs = require("fs");
 const DIR_TO_READ = "./demo";
+const DIR_TO_WRITE = "./demo/";
 const DEFAULT_DIR_NAME = "MY_DIR_";
-const DIR_PATH = "./demo/";
-let extensionsArray = [".txt", ".jpg", ".pdf", ".doc"];
-let dirArray = [
-  "./demo/autoDir_txt",
-  "./demo/autoDir_jpg",
-  "./demo/autoDir_pdf",
-  "./demo/autoDir_doc"
-];
 
 function move(oldPath, newPath, callback) {
   fs.rename(oldPath, newPath, function(err) {
@@ -40,7 +33,7 @@ function move(oldPath, newPath, callback) {
 
 function getDirForElement(element) {
   let index = element.lastIndexOf(".");
-  if(index === -1) return element ; 
+  if (index === -1) return element;
   let extension = element.substring(index + 1);
   console.log(extension);
   let dir = DEFAULT_DIR_NAME + extension;
@@ -49,42 +42,28 @@ function getDirForElement(element) {
 
 function makeDir(element) {
   let dir = getDirForElement(element);
-  let dirPath = DIR_PATH + dir; 
+  let dirPath = DIR_TO_WRITE + dir;
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
   }
-  // for (let i = 0; i < extensionsArray.length; i++) {
-  //   if (element.includes(extensionsArray[i])) {
-  //     console.log(element + " is of type" + extensionsArray[i]);
-  //     if (!fs.existsSync(dirArray[i])) {
-  //       fs.mkdirSync(dirArray[i]);
-  //     }
-  //   }
-  // }
 }
 
 function moveFile(element) {
-  for (let i = 0; i < extensionsArray.length; i++) {
-    if (element.includes(extensionsArray[i])) {
-      const elempath = DIR_TO_READ + "/" + element;
-      const newelempath = dirArray[i] + "/" + element;
-
-      console.log(element + " is moving from " + elempath);
-      move(elempath, newelempath, function(err) {
-        if (err) console.log(err);
-        console.log("...done");
-      });
-    }
-  }
+  const elempath = DIR_TO_READ + "/" + element;
+  let dir = getDirForElement(element);
+  if(dir === element) return; 
+  const newelempath = DIR_TO_WRITE + dir + "/"+  element;
+  console.log(element + " is moving from " + elempath + " to " + newelempath);
+  move(elempath, newelempath, function(err) {
+    if (err) console.log(err);
+    console.log("...done");
+  });
 }
 
 fs.readdir(DIR_TO_READ, (err, files) => {
   files.forEach(element => {
     console.log(element);
     makeDir(element);
-    // moveFile(element);
+    moveFile(element);
   });
 });
-// if (!fs.existsSync(dir)){
-//     fs.mkdirSync(dir);
-// }
