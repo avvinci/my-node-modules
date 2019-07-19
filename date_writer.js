ac5e4a673afac10a6971b4f4a034f13160df9917
avvinci <vinciabhinav7@gmail.com>
2019-07-19 14:45:31 +0530

    date generation somewhat working

--- date_writer.js
+++ date_writer.js
@@ -1,20 +1,30 @@
 var fs = require("fs");
 var stream = fs.createWriteStream("my_file.txt");
-// stream.once("open", function(fd) {
-stream.write("My first row\n");
-stream.write("My second row\n");
-// });
-
-// stream.once("open", function(fd) {
-stream.write("My first row\n");
-stream.write("My second row\n");
-// });
-
 const writer = stream;
-for (let i = 0; i < 100; i++) {
-  writer.write(`hello, #${i}!\n`);
+for (let i = 0; i < 30; i++) {
+  let date = new Date(2019, 06, 19 + i);
+  date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  writer.write(
+    `${date.getDate()} ${date.getMonthName()} ${date.getFullYear()}\n`
+  );
 }
 writer.end("This is the end\n");
 writer.on("finish", () => {
   console.log("All writes are now complete.");
 });
+
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+function getDates(startDate, stopDate) {
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(new Date(currentDate));
+    currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+}
